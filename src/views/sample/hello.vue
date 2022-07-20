d03423a3928770824b9aee2c3bc3e69a65ddd7c0
kimg0130 <kimg0130@gmail.com>
2022-07-20 20:32:44 +0900

    init

---
+++ src/views/sample/hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{message}}</h1>
+  <button @click="reverseMessage">reverse message</button>
+  <button @click="message += '!'">append !</button>
+  <a href="https://www.naver.com" @click.prevent="notify">naver</a>
+</template>
+
+<script setup>
+import {ref} from "vue";
+
+const message = ref('hello world!');
+
+function reverseMessage(){
+  message.value = message.value.split('').reverse().join('');
+}
+
+function notify(){
+  alert('hello');
+}
+</script>
+
+<style scoped>
+button,a{
+  display: block;
+}
+</style>
